4d1a69059eae0abc120faa5b7a498ebb43688461
Nutcra <92772397+Nutcra-yu@users.noreply.github.com>
2022-10-28 23:18:12 +0800

    Delete CPP/class/.history directory

--- CPP/class/.history/B6.8_20221028203614.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-class employee{};
-class employee{};
-class employee{};
-class employee{};
-class employee{};
--- CPP/class/.history/B6.8_20221028203654.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-class employee{};
-class manager:public employee{};
-class technician:public employee{};
-class saleman:public employee{};
